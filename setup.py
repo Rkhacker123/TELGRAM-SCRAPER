f6a1dcc78af3d152b593254577c6dad232dd886f
Rkhacker123 <68375950+Rkhacker123@users.noreply.github.com>
2020-07-29 08:00:51 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/bin/env python3
+# code by : youtube.com/theunknon
+
+"""
+
+you can re run setup.py 
+if you have added some wrong value
+
+"""
+import os, sys
+import configparser
+re="\033[1;31m"
+gr="\033[1;32m"
+cy="\033[1;36m"
+def banner():
+	os.system('clear')
+	print(f"""
+	{re}╔═╗{cy}┌─┐┌┬┐┬ ┬┌─┐
+	{re}╚═╗{cy}├┤  │ │ │├─┘
+	{re}╚═╝{cy}└─┘ ┴ └─┘┴
+	
+RECODED BY RK HACKER
+
+TO LEARN HACKING AND TO GET GIVEAWAYS JOIN t.me/pythonarmy
+
+                 version: 1.0
+               t.me/pythonarmy  
+              """)
+
+
+banner()
+print(gr+"[+] Installing requierments ...")
+os.system('python3 -m pip install telethon')
+os.system('pip3 install telethon')
+banner()
+os.system("touch config.data")
+cpass = configparser.RawConfigParser()
+cpass.add_section('cred')
+xid = input(gr+"[+] enter api ID : "+re)
+cpass.set('cred', 'id', xid)
+xhash = input(gr+"[+] enter hash ID : "+re)
+cpass.set('cred', 'hash', xhash)
+xphone = input(gr+"[+] enter phone number : "+re)
+cpass.set('cred', 'phone', xphone)
+setup = open('config.data', 'w')
+cpass.write(setup)
+setup.close()
+print(gr+"[+] setup complete !")
+print(gr+"[+] now you can run any tool !")
+print(gr+"[+] make sure to read docs 4 installation & api setup")
+print(gr+"[+] https://github.com/th3unkn0n/TeleGram-Scraper/blob/master/README.md")
